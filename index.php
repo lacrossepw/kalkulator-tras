c0b42a7048d57cd294ad7a5f8c4e75730d824058
lacrossepw <patrykoski@onet.pl>
2020-11-23 09:22:29 +0100

    aktualizacja

--- index.php
+++ index.php
@@ -4,8 +4,53 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" type="text/css" href="style.css" />
 </head>
 <body>
-    
 </body>
+<div class="container">
+    <header class="main-header">asdasd 
+        <nav class="navbar navbar-static-top" role="navigation">
+            <a class="sidebar-toggle" data-toggle="push-menu" role="button">
+                <span class="sr-only"></span>
+            </a>
+        </nav>
+    </header>
+    <aside class="main-sidebar"> 
+        <ul class="sidebar-menu tree">
+            <li>profil</li>
+            <li>moje trasy</li>
+            <li>nowy formularz</li>
+            <li>firma</li>
+            <li>kierowcy</li>
+
+        </ul>
+
+    </aside>
+   <div class="content" >
+    <div class="box box-success">
+        <div clss="box-header with-border"></div>
+        <div class="box-body">
+        <div class="callout callout-warning">
+                    <h4 class="box-title">Konwój</h4>
+                    <p>
+                        Konwój 22.11.2020 godzina 19:30 OBOWIĄZKOWA OBECNOŚĆ!                     </p>
+                </div>
+                <div class="callout callout-warning">
+                    <h4 class="box-title">Wykaz kilometrażu dot. doboru ciężarówki</h4>
+                    <p>
+                        0-15 000 km - Iveco 310 koni<br>
+15 001 - 45 000 - Renault Magnum 440 koni<br>
+45 001 - 70 000 - Man TGX 480 koni<br>
+70 001 - 90 000 - Daf Xf Super Space 550 koni<br>
+90 001 - 115 000 - Mercedes 625 koni<br>
+115 001 - ~  Własna ciężarówka<br>
+<br>
+Nie dostosowanie się do podanego obowiązku jazdy ciężarówką będzie konsekwencją odrzucania tras!                     </p>
+                </div>
+        </div> 
+    </div>
+    <div class="box box-success"> oś czasu </div>
+   <div> 
+</div>
 </html>
